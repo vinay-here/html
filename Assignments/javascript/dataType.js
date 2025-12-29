43b35635581ff167149ab362f14be0564546fa37
vinay-here <krvinay785@gmail.com>
2025-12-29 21:41:33 +0530

    dataType

---
+++ Assignments/javascript/dataType.js
@@ -0,0 +1,38 @@
+// String 
+
+let myName = "vinayKumar"
+console.log(myName);
+
+// Boolean
+
+let isCoder = true 
+console.log (isCoder);
+
+// Number 
+let age =23 
+console.log(age);
+
+// null
+
+let favFood = null 
+console.log (favFood);
+
+// undefined (kuch keh nahi skte)
+
+let favPlace;
+
+//Array
+let skills =["HTML","CSS","JS","JAVA"]
+console.log(skills);
+
+// ARRAY AND OBJECT ARE NON-PRIMITIVE DATA TYPES
+
+// Object 
+let student ={
+    myName :"Vinay",
+    age:23,
+    isCoder:true,
+    skills:["HTML","CSS","JS","C"],
+};
+
+console.log(typeof isCoder);
---
+++ Assignments/javascript/hello.js
@@ -0,0 +1,7 @@
+console.log ("good morning");
+
+function addNum (num1 ,num2) {
+    return num1 + num2;
+}
+
+console.log(addNum(3,4))
