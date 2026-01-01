ea7987d23e2207b4782c47deba603a5a3a4f8b82
vinay-here <krvinay785@gmail.com>
2026-01-01 23:59:17 +0530

    obj ques

---
+++ Assignments/javascript/objQues.js
@@ -0,0 +1,38 @@
+const teas ={
+    name: "lemon tea",
+    type :"green",
+    caffine:"low",
+
+}
+
+console.log(teas.name);
+console.log (teas["type"]);
+
+//Add new property
+teas.origin ="Assam"
+
+//change caffine level into medium
+teas.caffine="Medium"
+
+//remove teas property
+delete teas.type
+
+// check if property origin 
+console.log("origin"in teas);
+
+// use for loop to print all property of teas object
+
+for (let key in teas){
+    console.log(key, teas[key]);
+}
+
+//create nested object represents different type of teas
+
+const myTeas ={
+    greentea:{
+        name:"green tea",
+    },
+    blacktea:{
+        name:"black tea"
+    }
+}
