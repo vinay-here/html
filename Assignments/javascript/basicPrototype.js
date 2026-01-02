975357da6b3888f20721d303acebecb9347eaab2
vinay-here <krvinay785@gmail.com>
2026-01-02 23:13:27 +0530

    prototype basic

---
+++ Assignments/javascript/basicPrototype.js
@@ -0,0 +1,11 @@
+let arr =[1,2,3,4]
+
+let double =arr.map(r => r*2 )
+console.log(double);
+
+Array.prototype
+
+Array.prototype.vinay =function(){
+    console.log('My Name is Vinay');
+}
+Array.prototype // it work in console of browser
