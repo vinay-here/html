81d67ac497f3342882bb2d9daf0500c390a26adb
vinay-here <krvinay785@gmail.com>
2026-01-03 22:00:57 +0530

    polyfills

---
+++ Assignments/javascript/polyfills.js
@@ -0,0 +1,45 @@
+if(!Array.prototype.myForEach) {
+    Array.prototype.myForEach =function (userFun){
+     const OriginalArr=this;
+
+
+for (let i=0; i<OriginalArr.length;i++){
+    userFun(OriginalArr[i],i);
+}
+};
+}
+
+if (!Array.prototype.myMap){
+    Array.prototype.myMap=function(userFn){
+        const result =[]
+
+        for (let i=0; i<this.length; i++){
+            const val= userFn(this[i],i);
+        result.push(val);
+}
+        return result;
+    };
+}
+const arr =[1,2,3,4,5]
+
+// Error: .forEach fun doesn't exist on arr variable
+
+//Real signature pf forEach: No return, function as input , para as (value,index)
+//calls my fun for every value. 
+
+const Return =arr.myForEach(function(val,index){
+    console.log(`value at index ${index} is ${val}`);
+});
+console.log('Return is',Return);
+
+
+// Map 
+//return =new arr ,each element iterate ,userFun
+
+const n =arr.map(function(val,idx){
+    return val*2;
+
+});
+
+const n2= arr.myMap((e)=>e*3);
+console.log(n2);
